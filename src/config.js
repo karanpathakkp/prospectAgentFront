927b8cfec845895b0574dd2fcf30968d6612cab7
karanpathakkp <karanpathak@lambdatest.com>
2025-08-14 00:22:34 +0530

    updated

--- src/config.js
+++ src/config.js
@@ -4,7 +4,11 @@ const isNetlify = window.location.hostname.includes('netlify.app');
 
 // You can easily change the API endpoint here
 // Backend is running on 0.0.0.0:8000, but we need to use localhost for browser requests
-export const API_BASE_URL = 'http://localhost:8000';
+// export const API_BASE_URL = 'http://localhost:8000';
+
+export const API_BASE_URL = isDevelopment 
+  ? 'http://139.59.27.253:8000' 
+  : '/api';
 
 export const API_ENDPOINTS = {
   SEARCH: `${API_BASE_URL}/prospect/search`,
