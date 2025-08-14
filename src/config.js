58dac438bdf6c8649ab9c278c1530d62164f9b8c
karanpathakkp <karanpathak@lambdatest.com>
2025-08-14 13:33:12 +0530

    request address changed

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ const isNetlify = window.location.hostname.includes('netlify.app');
 
 // You can easily change the API endpoint here
 // Backend is running on 0.0.0.0:8000, but we need to use localhost for browser requests
-// export const API_BASE_URL = 'http://localhost:8000';
+// export const API_BASE_URL = 'http://localhost:80';
 
 export const API_BASE_URL = isDevelopment 
-  ? 'http://139.59.27.253:8000' 
+  ? 'http://139.59.27.253:80' 
   : '/api';
 
 export const API_ENDPOINTS = {
